291f90152e9cf12d101ed478a0fa1feeb17a95d9
Garrett Roberts <garrettmroberts@gmail.com>
2020-04-17 08:38:45 -0500

    reafactored app to use hooks

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Container from "./components/Container";
 import Row from "./components/Row";
 import Col from "./components/Col";
@@ -6,31 +6,26 @@ import Table from "./components/Table";
 import DropDown from "./components/DropDown";
 import employees from "./employees.json";
 
-class App extends Component {
-  state = {
-    orderBy: "id",
-    employees: employees
-  };
+function App() {
+  const [orderState, setOrderState] = useState('id');
+  const [employeesState, setEmployeesState] = useState(employees);
+  const [keysState, setKeysState] = useState(Object.keys(employeesState[0]));
 
-  keys = Object.keys(this.state.employees[0]);
-
-  render() {
-    return(
-      <div className="my-5">
-        <Container>
-          <Row>
-            <Col size="md-12" className="col-md-12 d-flex justify-content-center align-content-center">
-              <Table keys={this.keys} data={this.state.employees}>
-              </Table>
-            </Col>
-          </Row>
-            <DropDown name="sort" keys={this.keys} type="success" />
-            &nbsp;
-            <DropDown name="Filter" keys={this.keys}  type="secondary"/>
-        </Container>
-      </div>
-    )
-  }
+  return(
+    <div className="my-5">
+      <Container>
+        <Row>
+          <Col size="md-12" className="col-md-12 d-flex justify-content-center align-content-center">
+            <Table keys={keysState} data={employeesState}>
+            </Table>
+          </Col>
+        </Row>
+          <DropDown name="sort" keys={keysState} type="success" />
+          &nbsp;
+          <DropDown name="Filter" keys={keysState}  type="secondary"/>
+      </Container>
+    </div>
+  )
 }
 
 export default App;
