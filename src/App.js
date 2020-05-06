e48d652ccf0fe9b258a67fbf12a7ade7ba399d8f
Garrett Roberts <garrettmroberts@gmail.com>
2020-05-06 09:43:12 -0500

    added server directions

--- src/App.js
+++ src/App.js
@@ -1,71 +1,16 @@
-import React, { useState } from 'react';
-import Container from "./components/Container";
-import Row from "./components/Row";
-import Col from "./components/Col";
-import Table from "./components/Table";
-import DropDown from "./components/DropDown";
-import employees from "./employees.json";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./pages/Main";
 
 function App() {
-  const [orderState, setOrderState] = useState('id');
-  const [employeesState, setEmployeesState] = useState(employees);
-  const [keysState, setKeysState] = useState(Object.keys(employeesState[0]));
-
-  function departmentKeys() {
-    let keys = [];
-    employeesState.map(employee => {
-      keys.push(employee.department);
-    });
-    let uniqueKeys = Array.from(new Set(keys));
-    return uniqueKeys;
-  };
-
-  function reset() {
-    setEmployeesState(employees);
-  };
-
-  // Sorts or filters table based on type
-  function changeParams(type, affectedRow) {
-    if (type === "sort") {
-      setOrderState(affectedRow);
-      if (affectedRow === "salary" || affectedRow === "id") {
-        const query = `a.${affectedRow} - b.${affectedRow}`
-        const sorted = employeesState.sort((a,b) => eval(query)); 
-        console.log(sorted);
-      } else {
-        const query = `a.${affectedRow}.localeCompare(b.${affectedRow})`;
-        console.log(query);
-        const sorted = employeesState.sort((a,b) => eval(query));
-        console.log(sorted);
-      };
-    } else {
-      console.log(affectedRow);
-      const filtered = employeesState.filter((employee) => {
-        return employee.department === affectedRow
-      });
-      setEmployeesState(filtered);
-    }
-  };
-
   return(
-    <div className="my-5">
-      <Container>
-        <Row>
-          <h1>Employee Directory</h1>
-        </Row>
-        <Row>
-          <Col size="md-12" className="col-md-12 d-flex justify-content-center align-content-center">
-            <Table keys={keysState} data={employeesState} />
-          </Col>
-        </Row>
-
-          <DropDown name="sort" type="success" keys={keysState} func={changeParams} />
-          &nbsp;
-          <DropDown name="filter by department" type="secondary" keys={departmentKeys()} func={changeParams} />
-          &nbsp;
-          <button className="btn btn-danger btn-lg btn-block" onClick={() => reset()}>Reset</button>
-      </Container>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
