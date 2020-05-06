bde6c46962f1470b91bee9878af03e60743ea2a5
Garrett Roberts <garrettmroberts@gmail.com>
2020-05-06 10:03:00 -0500

    prepped for heroku deploy

---
+++ build/precache-manifest.ebef73573a322604d42b310ea4ddd5fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9549a0b20317919750be67728f4e83d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33464bc810950239ab34",
+    "url": "/static/js/2.dcfa801e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dcfa801e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1fc49cab7928a9a8368",
+    "url": "/static/js/main.eff8df62.chunk.js"
+  },
+  {
+    "revision": "4c33f4e9d35216f0fec7",
+    "url": "/static/js/runtime-main.b2401790.js"
+  }
+]);
